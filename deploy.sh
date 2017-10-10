4c82fd002cdc6019f80869ec83eeca09f562e27b
ros <rosalinesu@gmail.com>
2017-10-10 13:24:51 -0400

    edited deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 export PORT=8000
-DIR=$1
-
-if [ ! -d "$DIR" ]; then
-  printf "Usage: ./deploy.sh <path>\n"
-  exit
-fi
-
-echo "Deploy to [$DIR]"
 
 mix deps.get
 (cd assets && npm install)
@@ -16,11 +8,8 @@ mix deps.get
 mix phx.digest
 mix release --env=prod
 
-$DIR/bin/nu_mart stop || true
+./_build/prod/rel/microblog/bin/microblog stop || true
 
 mix ecto.migrate
 
-SRC=`pwd`
-(cd $DIR && tar xzvf $SRC/_build/prod/rel/nu_mart/releases/0.0.1/nu_mart.tar.gz)
-
-$DIR/SRC/_build/prod/rel/microblog start
+./_build/prod/rel/microblog/bin/microblog start
