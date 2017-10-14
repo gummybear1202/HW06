e14d9b05d94a62c239cbb2dfbd708f18e8af6f4b
Rosaline <rosalinesu@gmail.com>
2017-10-14 14:49:13 -0400

    fixing jquery

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -71,7 +71,7 @@ messageField.off("keypress").on("keypress", event => {
 })
 
 channel.on("new_msg", payload => {
-  feedsContainer.append(this.messageTemplate(payload))
+  feedsContainer.append(messageTemplate(payload))
 })
 
 // using part of the sample code from the phoenix example
