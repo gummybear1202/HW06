4a552820af5805e40911cb78c360068f8b8dd765
Rosaline <rosalinesu@gmail.com>
2017-10-14 15:12:13 -0400

    added the put-token in router

--- lib/microblog_web/plugs.ex
+++ lib/microblog_web/plugs.ex
@@ -7,8 +7,7 @@ defmodule MicroblogWeb.Plugs do
     user_id = get_session(conn, :user_id)
     if user_id do
       user = Microblog.Blog.get_user!(user_id)
-      token = Phoenix.Token.sign(conn, "user socket", user.id)
-      assign(conn, :current_user, user, :user_token, token)
+      assign(conn, :current_user, user)
     else
       assign(conn, :current_user, nil)
     end
