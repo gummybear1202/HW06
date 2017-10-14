b71225c70925721c130a3d2ebbf5541f988bb09f
Rosaline <rosalinesu@gmail.com>
2017-10-14 15:40:17 -0400

    showing user in chat 3

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -69,7 +69,7 @@ $( "#message-field" ).keypress(function() {
 
 messageField.off("keypress").on("keypress", event => {
   if(event.keyCode === 13) {
-    channel.push("new_msg", {user: u_email.val(), body: messageField.val()})
+    channel.push("new_msg", {user: u_email, body: messageField.val()})
     messageField.val("")
   }
 })
