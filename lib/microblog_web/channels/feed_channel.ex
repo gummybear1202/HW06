bb2c6faca2dbe422e606bc02cc3fb1ece97784dc
Rosaline <rosalinesu@gmail.com>
2017-10-15 21:09:23 -0400

    not using this

--- lib/microblog_web/channels/feed_channel.ex
+++ lib/microblog_web/channels/feed_channel.ex
@@ -3,19 +3,19 @@ defmodule MicroblogWeb.FeedChannel do
 
   def join("feed:lobby", payload, socket) do
     if authorized?(payload) do
-      send(self, {:join_record, payload})
+      # send(self, {:join_record, payload})
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
-  # notify joined successfully
-  def handle_info({:join_record, payload}, socket) do
-    broadcast! socket, "user:entered the chat", %{user: payload["user"]}
-    push socket, "join", %{status: "connected"}
-    {:noreply, socket}
-  end
+  # # notify joined successfully
+  # def handle_info({:join_record, payload}, socket) do
+  #   broadcast! socket, "user:entered the chat", %{user: payload["user"]}
+  #   push socket, "join", %{status: "connected"}
+  #   {:noreply, socket}
+  # end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
