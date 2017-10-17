ff248918ca7f035519883a21c63ded5b9798ae71
Rosaline <rosalinesu@gmail.com>
2017-10-16 21:59:53 -0400

    test

--- test/microblog_web/channels/updates_channel_test.exs
+++ test/microblog_web/channels/updates_channel_test.exs
@@ -11,14 +11,14 @@ defmodule MicroblogWeb.UpdatesChannelTest do
     {:ok, socket: socket}
   end
 
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push socket, "ping", %{"hello" => "there"}
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
+  # test "ping replies with status ok", %{socket: socket} do
+  #   ref = push socket, "ping", %{"hello" => "there"}
+  #   assert_reply ref, :ok, %{"hello" => "there"}
+  # end
 
   test "shout broadcasts to updates:lobby", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
+    push socket, "new_msg", %{"hello" => "all"}
+    assert_broadcast "new_msg", %{"hello" => "all"}
   end
 
   test "broadcasts are pushed to the client", %{socket: socket} do
