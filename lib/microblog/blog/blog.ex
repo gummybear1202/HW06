7516ad1c4e0a0b41ce544d69f8829bb86e05c36d
Rosaline <rosalinesu@gmail.com>
2017-10-19 18:57:20 -0400

    cleaning useless resource

--- lib/microblog/blog/blog.ex
+++ lib/microblog/blog/blog.ex
@@ -202,101 +202,101 @@ defmodule Microblog.Blog do
     User.changeset(user, %{})
   end
 
-  alias Microblog.Blog.Relation
-
-  @doc """
-  Returns the list of relations.
-
-  ## Examples
-
-      iex> list_relations()
-      [%Relation{}, ...]
-
-  """
-  def list_relations do
-    Repo.all(Relation)
-  end
-
-  @doc """
-  Gets a single relation.
-
-  Raises `Ecto.NoResultsError` if the Relation does not exist.
-
-  ## Examples
-
-      iex> get_relation!(123)
-      %Relation{}
-
-      iex> get_relation!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_relation!(id), do: Repo.get!(Relation, id)
-
-  @doc """
-  Creates a relation.
-
-  ## Examples
-
-      iex> create_relation(%{field: value})
-      {:ok, %Relation{}}
-
-      iex> create_relation(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_relation(attrs \\ %{}) do
-    %Relation{}
-    |> Relation.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a relation.
-
-  ## Examples
-
-      iex> update_relation(relation, %{field: new_value})
-      {:ok, %Relation{}}
-
-      iex> update_relation(relation, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_relation(%Relation{} = relation, attrs) do
-    relation
-    |> Relation.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Relation.
-
-  ## Examples
-
-      iex> delete_relation(relation)
-      {:ok, %Relation{}}
-
-      iex> delete_relation(relation)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_relation(%Relation{} = relation) do
-    Repo.delete(relation)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking relation changes.
-
-  ## Examples
-
-      iex> change_relation(relation)
-      %Ecto.Changeset{source: %Relation{}}
-
-  """
-  def change_relation(%Relation{} = relation) do
-    Relation.changeset(relation, %{})
-  end
+  # alias Microblog.Blog.Relation
+  #
+  # @doc """
+  # Returns the list of relations.
+  #
+  # ## Examples
+  #
+  #     iex> list_relations()
+  #     [%Relation{}, ...]
+  #
+  # """
+  # def list_relations do
+  #   Repo.all(Relation)
+  # end
+  #
+  # @doc """
+  # Gets a single relation.
+  #
+  # Raises `Ecto.NoResultsError` if the Relation does not exist.
+  #
+  # ## Examples
+  #
+  #     iex> get_relation!(123)
+  #     %Relation{}
+  #
+  #     iex> get_relation!(456)
+  #     ** (Ecto.NoResultsError)
+  #
+  # """
+  # def get_relation!(id), do: Repo.get!(Relation, id)
+  #
+  # @doc """
+  # Creates a relation.
+  #
+  # ## Examples
+  #
+  #     iex> create_relation(%{field: value})
+  #     {:ok, %Relation{}}
+  #
+  #     iex> create_relation(%{field: bad_value})
+  #     {:error, %Ecto.Changeset{}}
+  #
+  # """
+  # def create_relation(attrs \\ %{}) do
+  #   %Relation{}
+  #   |> Relation.changeset(attrs)
+  #   |> Repo.insert()
+  # end
+  #
+  # @doc """
+  # Updates a relation.
+  #
+  # ## Examples
+  #
+  #     iex> update_relation(relation, %{field: new_value})
+  #     {:ok, %Relation{}}
+  #
+  #     iex> update_relation(relation, %{field: bad_value})
+  #     {:error, %Ecto.Changeset{}}
+  #
+  # """
+  # def update_relation(%Relation{} = relation, attrs) do
+  #   relation
+  #   |> Relation.changeset(attrs)
+  #   |> Repo.update()
+  # end
+  #
+  # @doc """
+  # Deletes a Relation.
+  #
+  # ## Examples
+  #
+  #     iex> delete_relation(relation)
+  #     {:ok, %Relation{}}
+  #
+  #     iex> delete_relation(relation)
+  #     {:error, %Ecto.Changeset{}}
+  #
+  # """
+  # def delete_relation(%Relation{} = relation) do
+  #   Repo.delete(relation)
+  # end
+  #
+  # @doc """
+  # Returns an `%Ecto.Changeset{}` for tracking relation changes.
+  #
+  # ## Examples
+  #
+  #     iex> change_relation(relation)
+  #     %Ecto.Changeset{source: %Relation{}}
+  #
+  # """
+  # def change_relation(%Relation{} = relation) do
+  #   Relation.changeset(relation, %{})
+  # end
 
   alias Microblog.Blog.Follow
 
