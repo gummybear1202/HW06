ae6624e5b60188d2da943b98ea18dec19a489964
Rosaline <rosalinesu@gmail.com>
2017-10-22 15:29:27 -0400

    fix migration

--- priv/repo/migrations/20171022190527_auth.exs
+++ priv/repo/migrations/20171022190527_auth.exs
@@ -1,9 +1,8 @@
-# This file uses Nat Tuck's Nu Mart as a reference, details see
-# https://github.com/NatTuck/nu_mart/
 defmodule Microblog.Repo.Migrations.Auth do
   use Ecto.Migration
 
   def change do
+
     alter table("users") do
       add :authorized?, :boolean, null: false, default: false
     end
