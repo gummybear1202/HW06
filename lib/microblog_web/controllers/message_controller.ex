65caa33591e4476ee63c2de468f6879fe4eec14b
Rosaline <rosalinesu@gmail.com>
2017-10-21 22:24:19 -0400

    again stop redirection

--- lib/microblog_web/controllers/message_controller.ex
+++ lib/microblog_web/controllers/message_controller.ex
@@ -6,9 +6,7 @@ defmodule MicroblogWeb.MessageController do
 
   def index(conn, _params) do
     messages = Blog.list_messages()
-
-    changeset = Blog.change_message(%Message{})
-    render(conn, "index.html", messages: messages, changeset: changeset)
+    render(conn, "index.html", messages: messages)
   end
 
   def new(conn, _params) do
@@ -20,43 +18,43 @@ defmodule MicroblogWeb.MessageController do
     case Blog.create_message(message_params) do
       {:ok, message} ->
         conn
-        |> put_flash(:info, "Message created successfully.")
+        # |> put_flash(:info, "Message created successfully.")
+        # |> redirect(to: message_path(conn, :show, message))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    message = Blog.get_message!(id)
+    render(conn, "show.html", message: message)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    message = Blog.get_message!(id)
+    changeset = Blog.change_message(message)
+    render(conn, "edit.html", message: message, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "message" => message_params}) do
+    message = Blog.get_message!(id)
+
+    case Blog.update_message(message, message_params) do
+      {:ok, message} ->
+        conn
+        |> put_flash(:info, "Message updated successfully.")
         |> redirect(to: message_path(conn, :show, message))
-        {:error, %Ecto.Changeset{} = changeset} ->
-          render(conn, "new.html", changeset: changeset)
-        end
-      end
-
-      def show(conn, %{"id" => id}) do
-        message = Blog.get_message!(id)
-        render(conn, "show.html", message: message)
-      end
-
-      def edit(conn, %{"id" => id}) do
-        message = Blog.get_message!(id)
-        changeset = Blog.change_message(message)
+      {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", message: message, changeset: changeset)
-      end
+    end
+  end
 
-      def update(conn, %{"id" => id, "message" => message_params}) do
-        message = Blog.get_message!(id)
+  def delete(conn, %{"id" => id}) do
+    message = Blog.get_message!(id)
+    {:ok, _message} = Blog.delete_message(message)
 
-        case Blog.update_message(message, message_params) do
-          {:ok, message} ->
-            conn
-            |> put_flash(:info, "Message updated successfully.")
-            |> redirect(to: message_path(conn, :show, message))
-            {:error, %Ecto.Changeset{} = changeset} ->
-              render(conn, "edit.html", message: message, changeset: changeset)
-            end
-          end
-
-          def delete(conn, %{"id" => id}) do
-            message = Blog.get_message!(id)
-            {:ok, _message} = Blog.delete_message(message)
-
-            conn
-            |> put_flash(:info, "Message deleted successfully.")
-            |> redirect(to: message_path(conn, :index))
-          end
-        end
+    conn
+    |> put_flash(:info, "Message deleted successfully.")
+    |> redirect(to: message_path(conn, :index))
+  end
+end
