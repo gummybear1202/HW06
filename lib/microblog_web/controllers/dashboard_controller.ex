f1388ffb782feda45164d256489aee773d6ca744
ros <rosalinesu@gmail.com>
2017-10-23 18:09:22 -0400

    deleted dashboard that was never used

--- lib/microblog_web/controllers/dashboard_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule MicroblogWeb.DashboardController do
-  use MicroblogWeb, :controller
-
-  def show(conn, _params) do
-    render(conn, "show.html")
-  end
-end
--- lib/microblog_web/views/dashboard_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule MicroblogWeb.DashboardView do
-  use MicroblogWeb, :view
-end
