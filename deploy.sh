4687f5380fae3355f5f5203214d14db735131852
ros <rosalinesu@gmail.com>
2017-10-23 21:05:34 -0400

    edit seeds

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,12 @@ mix deps.get
 (cd assets && npm install)
 (cd assets && ./node_modules/brunch/bin/brunch b -p)
 mix phx.digest
+mix run priv/repo/seeds.exs
 mix release --env=prod
 
 ./_build/prod/rel/microblog/bin/microblog stop || true
 
 mix ecto.migrate
+
 
 ./_build/prod/rel/microblog/bin/microblog start
